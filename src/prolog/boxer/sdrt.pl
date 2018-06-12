dd48ff46bd1aef6b9d1e1c331bf25e4c2797be25
Jonathan Gordon <jgordon@isi.edu>
2018-06-12 10:23:15 -0700

    Edit whitespace.

--- src/prolog/boxer/sdrt.pl
+++ src/prolog/boxer/sdrt.pl
@@ -2,7 +2,7 @@
 :- module(sdrt,[mergeSDRS/2]).
 
 :- use_module(library(lists),[member/2,append/3]).
- 
+
 
 /* =========================================================================
    Subordinate SDRT relations
@@ -38,7 +38,7 @@ mergeSDRS(smerge(S1,S2,Rel,_Pops),S3):-
 mergeSDRS(smerge(S1,S2,Rel,_Pops),S3):-
    coordinate(Rel), !,
    cooDRS(S1,Rel,S2,S3).
-  
+
 
 /* =========================================================================
    Insert subordinate DRS into SDRS
@@ -60,7 +60,7 @@ subDRS(sdrs([sub(L1,lab(L2,S1))],R),Rel,B,sdrs([sub(L1,lab(L2,S2))],R)):- !,
 subDRS(sdrs([lab(K1,B1)],R),Rel,B2,SDRS):- !,
    SDRS = sdrs([sub(lab(K1,B1),lab(K2,B2))],[[]:rel(K1,K2,Rel)|R]).
 
-% Recurse down to last label of first argument  
+% Recurse down to last label of first argument
 %
 subDRS(sdrs([X|L1],R1),Rel,New,sdrs([X|L2],R2)):-
    subDRS(sdrs(L1,R1),Rel,New,sdrs(L2,R2)).
@@ -72,12 +72,12 @@ subDRS(sdrs([X|L1],R1),Rel,New,sdrs([X|L2],R2)):-
 
 % Make SDRSs of both DRSs (if needed)
 %
-cooDRS(B1,Rel,B2,S):- 
-   member(B1,[_:_,merge(_,_),alfa(_,_,_)]), !, 
+cooDRS(B1,Rel,B2,S):-
+   member(B1,[_:_,merge(_,_),alfa(_,_,_)]), !,
    cooDRS(sdrs([lab(_,B1)],[]),Rel,B2,S).
 
-cooDRS(B1,Rel,B2,S):- 
-   member(B2,[_:_,merge(_,_),alfa(_,_,_)]), !, 
+cooDRS(B1,Rel,B2,S):-
+   member(B2,[_:_,merge(_,_),alfa(_,_,_)]), !,
    cooDRS(B1,Rel,sdrs([lab(_,B2)],[]),S).
 
 % Non-SUB + Non-SUB
@@ -109,12 +109,8 @@ cooDRS(sdrs([lab(K1,B1)],R1),Rel,sdrs([sub(lab(K2,B2),B4)|L],R2),SDRS):- !,
    append(R1,[[]:rel(K1,K2,Rel)|R2],R3),
    SDRS = sdrs([lab(K1,B1),sub(lab(K2,B2),B4)|L],R3).
 
-% Recurse down to last label of first argument  
+% Recurse down to last label of first argument
 %
 cooDRS(sdrs([X|L1],R1),Rel,New,sdrs([X|L2],R2)):- !,
    cooDRS(sdrs(L1,R1),Rel,New,sdrs(L2,R2)).
-
-
-
-
 
