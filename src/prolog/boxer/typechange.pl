d6d1d81cb9fe48eb197abe488df46a8b0e7d9915
Jonathan Gordon <jgordon@jonathangordon.org>
2018-06-21 09:34:27 -0700

    Update typechange.pl.

--- src/prolog/boxer/typechange.pl
+++ src/prolog/boxer/typechange.pl
@@ -30,49 +30,48 @@
 typechange(Old,Phi,_,New,app(Psi,Phi)):- 
    member(Old,[s:_\np,s:_/np]), 
    New = np\np, 
-   semlex(New/Old,that,[],[]-[],Psi), !.
+   semlex(New/Old,that,[],[]-_,Psi), !.
 
 % Ex: an effort [ ... ] to end the violence
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):- 
    member(Old,[s:_\np,s:_/np]), 
    member(New,[n/n,n\n]), 
-   semlex(New/Old,that,[],[]-[],Psi), !.
+   semlex(New/Old,that,[],[]-_,Psi), !.
 
 % Ex: a sign [that] the effort is working 
 % 
 typechange(Old,Phi,_,New,app(Psi,Phi)):- 
    Old = s:_,
    New = np\np, 
-   semlex(New/Old,that,[],[]-[],Psi), !.
+   semlex(New/Old,that,[],[]-_,Psi), !.
 
 % Ex: sign [that] the effort is working 
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):- 
    Old = s:_,
    New = n\n, 
-   semlex(New/Old,that,[],[]-[],Psi), !.
+   semlex(New/Old,that,[],[]-_,Psi), !.
 
 % <example missing>
 %
-typechange(Old/np,Phi,_,New,app(Psi,app(Phi,NP))):-
-   member(Old,[s:_\np,s:_/np]), 
+typechange(Old,Phi,_,New,app(Psi,Phi)):-
+   Old = (s:_\np)/np, 
    member(New,[np/np,np\np]),
-   semlex(New/Old,empty,[],[]-[],Psi), !,
-   NP = lam(P,merge(B:drs([B:[]:X],[B:[]:pred(X,thing,n,12)]),app(P,X))).
+   semlex(New/Old,empty,[],[]-_,Psi), !.
 
 % Ex: walking [in order] to get fit
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):- 
    Old = s:_\np,
    member(New,[(s:X\np)\(s:X\np), (s:X\np)/(s:X\np)]),
-   semlex(New/Old,for,[],[]-[],Psi), !.
+   semlex(New/Old,for,[],[]-_,Psi), !.
 
 % Ex: [the] man
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):-
    Old = n, New = pn,                     %%% preserved for Kilian
-   semlex(New/Old,the,[],[]-[],Psi), !.
+   semlex(New/Old,the,[],[]-_,Psi), !.
 
 % Ex: [the] men
 %
@@ -80,20 +79,20 @@ typechange(Old,Phi,Att,New,app(Psi,Phi)):-
    Old = n, New = np,
    att(Att,pos,POS), 
    member(POS,['NNP','NNPS']), 
-   semlex(New/Old,the,[],[]-[],Psi), !.
+   semlex(New/Old,the,[],[]-_,Psi), !.
 
 % Ex: [some] men
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):-
    Old = n, New = np,
-   semlex(New/Old,some,[],[]-[],Psi), !.
+   semlex(New/Old,some,[],[]-_,Psi), !.
 
 % Ex: there is hope [and] the rain will end
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):-
    Old = s:_, 
    member(New,[s:X\s:X,s:X/s:X]),
-   semlex(New/Old,and,[],[]-[],Psi), !.
+   semlex(New/Old,and,[],[]-_,Psi), !.
 
 % Ex: (they say) [the event of] running a marathon (helps)
 %
@@ -108,12 +107,12 @@ typechange(Old,Phi,_,New,app(Psi,Phi)):-
 typechange(Old,Phi,_,New,app(Psi,Phi)):-
    Old = s:_\np, 
    member(New,[s:X/s:X,s:X\s:X]),
-   semlex(New/Old,while,[],[]-[],Psi), !.
+   semlex(New/Old,while,[],[]-_,Psi), !.
 
 % General rule, could be further instantiated (rel)
 %
 typechange(Old,Phi,_,New,app(Psi,Phi)):-
-   semlex(New/Old,rel,[],[]-[],Psi), !,
+   semlex(New/Old,rel,[],[]-_,Psi), !,
    warning('type changing for ~p',[New/Old]).
 
 %typechange(np,Phi,_, (s:X/s:X),app(New,Phi)):- !,
