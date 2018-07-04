259831d55b4f0eb08225b02060b44d3e1e15e064
Jonathan Gordon <jgordon@jonathangordon.org>
2018-07-04 08:59:50 -0700

    Update ne.pl, in progress.

--- src/prolog/boxer/knowledge/ne.pl
+++ src/prolog/boxer/knowledge/ne.pl
@@ -1,5 +1,4 @@
-
-:- module(ne,[neClass/2,neClassType/3]).
+:- module(ne,[neClass/2,neClassType/3,neClassType/4]).
 
 neClassType('I-LOC',loc,nam):- !.
 neClassType('B-LOC',loc,nam):- !.
@@ -21,14 +20,22 @@ neClassType('I-TIM',tim,nam):- !.
 neClassType('B-TIM',tim,nam):- !.
 neClassType('E-TIM',tim,nam):- !.
 
-neClassType('Person',per,nam):- !.
-neClassType('Organization',org,nam):- !.
-neClassType('Location',geo,nam):- !.
-neClassType('Artifact',art,nam):- !.
-neClassType('Event',eve,nam):- !.
-neClassType('Natural_Object',nat,nam):- !.
-neClassType('Time',tim,nam):- !.
-neClassType('GPE',gpe,nam):- !.
+neClassType('I-MON',geo,nam,'UOM'):- !.
+neClassType('B-MON',geo,nam,'UOM'):- !.
+neClassType('E-MON',geo,nam,'UOM'):- !.
+
+neClassType('Person',per,nam,'PER'):- !.
+neClassType('Organization',org,nam,'ORG'):- !.
+neClassType('Location',geo,nam,'LOC'):- !.
+neClassType('Artifact',art,nam,'ART'):- !.
+neClassType('Event',eve,nam,'HAP'):- !.
+neClassType('Natural_Object',nat,nam,'NAT'):- !.
+neClassType('Time',tim,nam,'TIM'):- !.
+neClassType('GPE',gpe,nam,'GPE'):- !.
+
+neClassType(N,Class,Type,Tag):- atom(N), atomic_list_concat([Class,Type],'-',N), neClassType(_,Class,_,Tag), !.
+neClassType(_,nam,nam,'UNK'):- !.
+neClassType(_,_,_,'UNK').
 
 neClassType(N,Class,Type):- atom(N), atomic_list_concat([Class,Type],'-',N), !.
 neClassType(_,nam,nam).
